e572c20b97b4dfc6902720835048a5980098f0de
MatheusLopes21 <matheusthiagoal@hotmail.com>
2025-05-21 23:27:26 -0300

    test(validação): adiciona teste para letra maiúscula

---
+++ ValidadorSenha.java
@@ -0,0 +1,9 @@
+
+public class ValidadorSenha {
+
+    public Object isValid(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isValid'");
+    }
+
+}
---
+++ validador_senhas_teste.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class validador_senhas_teste {
+
+    ValidadorSenha validador = new ValidadorSenha();
+
+    @Test
+    public void testHasUppercase() {
+        assertFalse(validador.isValid("senha123"));   // nenhuma maiúscula
+        assertTrue(validador.isValid("Senha123"));    // tem maiúscula
+    }
+
