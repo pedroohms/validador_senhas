a70f1f0f3bbba71f0557d42aaec49f8481095fb8
Arthur <arthursleandrk2002@gmail.com>
2025-05-22 13:52:07 -0300

    Adiciona testes Funcionas!

---
+++ ValidandoSenha.java
@@ -0,0 +1,8 @@
+public class ValidandoSenha{
+
+    public boolean isValid(String password) {
+        return password.length() >= 8 &&
+               password.matches(".[A-Z].") &&
+               password.matches(".\\d.");
+    }
+}
